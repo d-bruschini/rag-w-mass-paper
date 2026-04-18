87649db4c4d1eabe66f0597057e67f5f77f48c95
Davide Bruschini <brsk2ktm@gmail.com>
2026-04-18 18:15:22 +0200

    Use \'page_label\' instead of \'page\' in context

--- src/retrieval.py
+++ src/retrieval.py
@@ -3,11 +3,6 @@ import numpy as np
 from config import TOP_K
 from embeddings import create_embeddings
 
-def format_page(page):
-    roman_map = {-1: "i", 0: "ii"}
-    
-    return roman_map.get(page - 1, page - 1)
-
 def normalize(embeddings):
     # Normalize embeddings to 1 and use float32 precision (normalize_L2 works for dtype float32. Also, no change was observed when the precision is reduced from float64 in the default configuration)
     embeddings_f32 = np.array(embeddings, dtype=np.float32)
@@ -35,8 +30,7 @@ def create_context(query, index, docs):
     title = docs[0].metadata.get("title","unknown")
     context = f"Title: {title}\n\n"
     for i in top_idx:
-        page = docs[i].metadata.get("page","unknown")
-        page = format_page(page)
+        page = docs[i].metadata.get("page_label","unknown")
         text = docs[i].page_content
         context += f"[Page {page}]\n{text}\n\n" # we could also pass the chunk index, but this would be mostly for debugging, rather than to cross-check on the actual original paper
     return context
